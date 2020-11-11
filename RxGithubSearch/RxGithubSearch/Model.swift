8bf6ebc7afb2659909cdc1c220f6c72e85145c53
rookie.w <wanrage@naver.com>
2020-11-11 19:47:51 +0900

    SearchRepogitoriesResults 모델 추가

---
+++ RxGithubSearch/RxGithubSearch/Model.swift
@@ -0,0 +1,17 @@
+//
+//  Model.swift
+//  RxGithubSearch
+//
+//  Created by rookie.w on 2020/11/11.
+//
+
+import Foundation
+
+struct searchRepogitoriesResults {
+    let acccept: String
+    let q: String
+    let sort: String
+    let order: String
+    let per_page: Int
+    let page: Int
+}
