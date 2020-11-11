3e14b1099bf762b5e471d941120c6c3840a12800
rookie.w <wanrage@naver.com>
2020-11-11 19:32:27 +0900

    스토리보드 제거

---
+++ RxGithubSearch/RxGithubSearch/GithubSearchViewController.swift
@@ -0,0 +1,14 @@
+//
+//  GithubSearchViewController.swift
+//  RxGithubSearch
+//
+//  Created by rookie.w on 2020/11/11.
+//
+
+import UIKit
+
+class GithubSearchViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
