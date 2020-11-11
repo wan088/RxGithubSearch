34260e42846255eede87b226d4c9ed82aac0d6f4
rookie.w <wanrage@naver.com>
2020-11-11 18:41:49 +0900

    Create Project

---
+++ RxGithubSearch/RxGithubSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxGithubSearch
+//
+//  Created by rookie.w on 2020/11/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
