b343aca2c81fdb4aaaab26a4cd1ea2b173b56ec6
rookie.w <wanrage@naver.com>
2020-11-19 20:40:55 +0900

    testSearchRepogitories_whenNetworkFail_getNil 테스트코드 작성

--- RxGithubSearch/RxGithubSearchTests/APITests.swift
+++ RxGithubSearch/RxGithubSearchTests/APITests.swift
@@ -59,27 +59,46 @@ final class APITests: XCTestCase {
         XCTAssertEqual(myResult!.total_count, 3)
     }
     
-//    func testSearchRepogitories_whenNetworkFail_getNil () {
-//        //given
-//
-//        let stubbedResults: [String : Any]?
-//        //when - getSerachRepogitories API 호출
-//
-//        let result: SearchRepogitoriesResults? = nil
-//
-//        //then
-//        XCTAssertNil(result)
-//    }
-//
-//    func testSearchRepogitories_whenWrongDataFail_getNil () {
-//        //given
-//
-//        let stubbedResults: [String : Any]?
-//        //when - getSerachRepogitories API 호출
-//
-//        let result: SearchRepogitoriesResults? = nil
-//
-//        //then
-//        XCTAssertNil(result)
-//    }
+    func testSearchRepogitories_whenNetworkFail_getNil () {
+        //given
+        let urlSessionSpy = URLSessionSpy()
+        urlSessionSpy.stubbedSearchRepositoriesResultsString = """
+            {
+              "total_count": 3,
+              "incomplete_results": false,
+              "items": [
+                    {
+                      "id": 26262860,
+                      "node_id": "MDEwOlJlcG9zaXRvcnkyNjI2Mjg2MA==",
+                      "name": "wangEditor",
+                      "full_name": "wangeditor-team/wangEditor"
+                    },
+                    {
+                      "id": 26262860,
+                      "node_id": "MDEwOlJlcG9zaXRvcnkyNjI2Mjg2MA==",
+                      "name": "wangEditor",
+                      "full_name": "wangeditor-team/wangEditor"
+                    },
+                    {
+                      "id": 26262860,
+                      "node_id": "MDEwOlJlcG9zaXRvcnkyNjI2Mjg2MA==",
+                      "name": "wangEditor",
+                      "full_name": "wangeditor-team/wangEditor"
+                    }
+                ]
+            }
+        """
+        urlSessionSpy.stubbedError = ErrorDummy()
+        let api = API(urlSession: urlSessionSpy)
+        var myResult: SearchRepogitoriesResults! = SearchRepogitoriesResults(total_count: 3, incomplete_results: true, items: [])
+        
+        //when - getSerachRepogitories API 호출
+        
+        let result = api.getRepogitoriesResults(keyword: "wan", sort: .stars, order: .asc) { (result) in
+            myResult = result
+        }
+        
+        //then
+        XCTAssertNil(myResult)
+    }
 }
