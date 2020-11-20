3bade188d508b5af1add25c2c29deb398b23016a
rookie.w <wanrage@naver.com>
2020-11-20 17:32:18 +0900

    testSearchResult_whenSuccess() 테스트 코드 작성

--- RxGithubSearch/RxGithubSearchTests/SearchControllerTest.swift
+++ RxGithubSearch/RxGithubSearchTests/SearchControllerTest.swift
@@ -12,7 +12,28 @@ import XCTest
 class SearchControllerTest: XCTestCase {
     
     func testSearchResult_whenSuccess() {
-
+        // given
+        let controller = SearchController()
+        let api = APISpy()
+        api.stubbedSearchRepogitoriesResults = SearchRepogitoriesResults(
+            total_count: 3,
+            incomplete_results: true,
+            items: [
+                Repogitory(id: 1, node_id: "id1", name: "dfg", full_name: "dfg"),
+                Repogitory(id: 2, node_id: "id2", name: "dfggd", full_name: "dfg"),
+                Repogitory(id: 3, node_id: "id3", name: "dfgfs", full_name: "dfg"),
+            ]
+        )
+        controller.api = api
+        controller.view.layoutIfNeeded()
+        
+        // when
+        controller.navigationItem.searchController?.searchBar.text = "wan"
+        
+        // then
+        XCTWaiter().wait(for: [XCTestExpectation()], timeout: 1)
+        XCTAssertEqual(controller.tableView.numberOfRows(inSection: 0), 3)
+        XCTAssertEqual(controller.tableView.cellForRow(at: IndexPath(row: 0, section: 0))?.textLabel?.text, "dfg")
     }
     
     func testSearchToggleBtn_whenTapped() {
@@ -27,4 +48,6 @@ class SearchControllerTest: XCTestCase {
         controller.toggleSearchType(3)
         XCTAssertEqual(controller.title, "\(searchType.next().next()) _ Search")
     }
+    
+    
 }
